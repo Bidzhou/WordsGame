807b6966fd767278a82dec202815265c15bf96b7
Bidzhou <89260226211@mail.ru>
2024-04-22 17:46:22 +0300

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Bit Bitin on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
